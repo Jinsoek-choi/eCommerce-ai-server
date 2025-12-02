3805d7632c27d3bcea5bec553b38bf3571334eee
yuha <dladbgkdla@gmail.com>
2025-12-02 11:06:14 +0900

    * .gitignore : .env 추가 main.py : test용 코드 requirements.txt : openAI, dotenv 패키지 설치 ai_server_core.py : 상품 정보를 받아서 OpenAI에게 "상품 상세 설명"을 생성 요청하는 함수 작성 server.py : FastAPI 페이지(AI 기능만 담당하는 별도 REST API 서버)   - OpenAI에게 상품 설명 생성 요청 보내기   -  AI가 준 결과를 JSON으로 프론트/백엔드에게 전달

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-# FastAPI 서버 시작점
-from fastapi import FastAPI, UploadFile, File
+from ai_server_core import generate_product_description
 
-app = FastAPI()
+# 테스트용 main
 
+def main():
+    product = {
+        "name": "Angel Star Patch 자수 원턱 스트링 와이드 스웨트 팬츠 AP903 (블랙)",
+        "price": 30320,
+        "options": "색상: 화이트/블랙/멜란지그레이, 사이즈: M/L/XL",
+        "category_path": "바지 > 트레이닝/조거 팬츠(엠블러)",
+        "image_url": "https://image.msscdn.net/thumbnails/images/goods_img/20250819/5338392/5338392_17564331759079_big.jpg?w=1200",
+    }
+
+    text = generate_product_description(product)
+    print("===== 생성된 상품 상세 설명 =====")
+    print(text)
+    print("================================")
+
+if __name__ == "__main__":
+    main()
+    
+# 실행 시 python main.py
